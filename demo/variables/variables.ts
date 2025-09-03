51503979c4cac97045704e1784f687ee426fd491
Ash <ash.krawczyk@hbk-essen.de>
2025-09-03 15:17:07 +0200

    exampels for variables

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -9,3 +9,57 @@
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
 //
+
+
+const courseName = "typescript";
+const courseName2 = 'typescript';
+
+//it doesn't matter if u use '' or ""
+
+const CourseName3 = `typescript`;
+
+//backquotes are used a different way take a look at template-strings
+
+
+
+const amount = 10;
+const fraction = 10.5;
+
+const oneThousand = 1e3;
+//Exponent
+
+const allPermissions = 0o777; 
+//can be used on Linux
+
+const hexByte = 0xff ;
+
+const binary = 0b0100001;
+
+const bigInt = 9000n;
+
+const yes = true;
+const no = false;
+//booleans
+
+const missing = undefined;
+//when something is missing and we know nothing about it
+
+const empty = null;
+//we know its missing
+
+let someNum = 0;
+someNum = 1;
+someNum = 2;
+
+//if you want to change a variable use "let"
+//but u can't use let multipeltypes
+
+//VARIABLE SHADOWING
+//is used for testing
+
+{
+    let someNum = 5;
+}
+
+let hello;
+//u don't need to initalize variables, can be usefull if u want to set ur variable later but u need to use let and not const 
