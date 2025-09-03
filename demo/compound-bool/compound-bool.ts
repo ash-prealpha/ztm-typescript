613435080fd6921a24f6a3a04fcfee508ffaf30c
Ash <ash.krawczyk@hbk-essen.de>
2025-09-03 16:34:29 +0200

    example for compound bools

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,4 +1,34 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+// NOT
 
+const writing = true;
+const reading = !writing;
+
+// "!" not; switch between true and false
+
+// OR
+
+const rating = 9;
+const favoriteMovie = false;
+
+const suggestMovie = rating > 8 || favoriteMovie;
+assert.equal (suggestMovie, true);
+
+
+// AND
+
+const age = 18;
+const isTeenager = age >= 13 && age < 20;
+assert.equal (isTeenager, true);
+
+
+
+const packageWeight = 30;
+const packageLength = 50;
+const feeExemption = false;
+
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
+
+//if you have more than 1 AND or more than 1 OR  u should begin to work with functions
