6032878a65639746efa3e999cc62dec811f526c5
Ash <ash.krawczyk@hbk-essen.de>
2025-09-04 17:19:43 +0200

    Exercise | Function 2.ts

--- exercise/functions/2.ts
+++ exercise/functions/2.ts
@@ -17,6 +17,28 @@
 
 import { strict as assert } from "assert";
 
+const RectangleLength : number = 5;
+const RectangleWith : number = 10;
+const Circeldiameter : number = 7;
+
+function calculateRectangleArea (RectangleLength : number , RectangleWith : number ) : number
+{
+    const RectangleArea = RectangleLength * RectangleWith;
+    return RectangleArea; 
+}
+
+function calculateRectanglePerimeter (RectangleLength : number, RectangleWith : number) : number
+{
+    const RectanglePerimeter = 2 * (RectangleLength + RectangleWith);
+    return RectanglePerimeter;
+}
+
+function calculateCircleArea (Circeldiameter : number) : number
+{
+    const pi = 3.14;
+    const CircleArea = pi * Circeldiameter * Circeldiameter;
+    return CircleArea;
+}
 
 //
 // Test cases
@@ -24,3 +46,13 @@ assert.equal(calculateRectangleArea(5, 10), 50);
 assert.equal(calculateRectanglePerimeter(5, 10), 30);
 assert.equal(calculateCircleArea(7), 153.86);
 
+const rectanglearea = calculateRectangleArea (RectangleLength, RectangleWith);
+const rectangleperimeter = calculateRectanglePerimeter (RectangleLength, RectangleWith);
+const circlearea = calculateCircleArea (Circeldiameter);
+
+console.log (`Rectangle Length: ${RectangleLength}`);
+console.log (`Rectangle With: ${RectangleWith}`);
+console.log (`Rectangle Area: ${rectanglearea}`);
+console.log (`Rectangle Perimeter: ${rectangleperimeter}`);
+console.log (`Circeldiameter: ${Circeldiameter}`);
+console.log (`Circle Area: ${circlearea}`);
