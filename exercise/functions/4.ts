207ce65c71bec80db24339b9efddc70464c3aece
Ash <ash.krawczyk@hbk-essen.de>
2025-09-05 12:15:15 +0200

    Exercise | Functions 4

--- exercise/functions/4.ts
+++ exercise/functions/4.ts
@@ -17,6 +17,22 @@
 
 import { strict as assert } from "assert";
 
+function isSenior (age : number) : boolean
+{
+    return age > 65;
+};
+
+function isLargePurchase (amount : number) : boolean
+{
+    return amount > 99;
+};
+
+function isEligibleForDiscount (ageSenior, Purchaseamount) : boolean
+{
+ const Seniordiscount = isSenior (ageSenior);
+ const Purchasdiscount = isLargePurchase (Purchaseamount);
+ return Seniordiscount || Purchasdiscount;
+}
 
 // 
 // Test cases
@@ -24,3 +40,6 @@ assert.equal(isEligibleForDiscount(60, 90), false);
 assert.equal(isEligibleForDiscount(70, 90), true);
 assert.equal(isEligibleForDiscount(60, 100), true);
 
+console.log (`Eligibal for Discount? - Age: 60, Purchaseamount: 90 - ${isEligibleForDiscount(60,90)}`);
+console.log (`Eligibal for Discount? - Age: 70, Purchaseamount: 90 - ${isEligibleForDiscount(70,90)}`);
+console.log (`Eligibal for Discount? - Age: 60, Purchaseamount: 100 - ${isEligibleForDiscount(60,100)}`);
