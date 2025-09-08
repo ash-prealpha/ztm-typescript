bcc3dddb23d59ac86be72ed7da3a9f7938e9dc40
Ash <ash.krawczyk@hbk-essen.de>
2025-09-08 13:58:24 +0200

    Example | Arry - Object

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,3 +1,26 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+type Link = 
+{
+    tittle : string;
+    url: string;
+};
+
+const microsoft =
+{
+    title: "microsoft",
+    url: "microsoft.com",
+};
+
+const typescript =
+{
+    title: "TypeScript",
+    url: "tyypescript",
+};
+
+const links = [microsoft, typescript];
+
+const tsUrl = links[1].url ;
+
+links[0].title = "Microsoft";
