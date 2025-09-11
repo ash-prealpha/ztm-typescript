aa6bf632bde7afef81fe0bb0ab2cdff74aada7d3
Ash <ash.krawczyk@hbk-essen.de>
2025-09-11 18:33:42 +0200

    Example | optional - fields

--- demo/optional-fields/optional-fields.ts
+++ demo/optional-fields/optional-fields.ts
@@ -1,5 +1,7 @@
 /* eslint-disable */
 
+import { _ } from "core-js";
+
 // You can define optional fields in your object types. Optional fields are
 // fields that may or may not be present in an object. You can make a field
 // optional by appending a question mark "?" to its name in the type
@@ -10,3 +12,53 @@
 // https://www.typescriptlang.org/docs/handbook/2/objects.html#optional-properties
 
 
+type Warranty = "standard" | "extended";
+
+function warrantyInfo(warranty: Warranty): string
+{
+    switch (warranty)
+    {
+        case "standard":
+            return "90 day warranty";
+        case "extended":
+            return "180 day extended warranty";
+    }
+}
+
+interface LineItem
+{
+    name: string;
+    quantity: number;
+    //"undefined" if not present
+    warranty?: Warranty;
+}
+
+function printLine(item: LineItem): void
+{
+    console.log(`Item ${item.name}`);
+    console.log(`Quantitiy ${item.quantity}`);
+
+    if (item.warranty !== undefined)
+    {
+     console.log(`Warranty ${warrantyInfo(item.warranty)}`);   
+    }
+    else
+    {
+        console.log("Warranty: None");
+    }
+}
+
+const boxFan: LineItem =
+{
+    name: "box fan",
+    quantity: 1,
+}
+printLine(boxFan);
+
+const heater: LineItem =
+{
+    name: "heater",
+    quantity: 1,
+    warranty: "standard"
+}
+printLine(heater);
